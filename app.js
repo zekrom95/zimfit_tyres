40933a0eb73da7f68e9ad74c155f7b894f746a43
Runyararo Mucheche <wmucheche@gmail.com>
2020-10-13 13:09:09 +0000

    add extra pages and edit main files

--- app.js
+++ app.js
@@ -5,8 +5,20 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
+	res.render("landing");
+});
+
+app.get("/home", function(req, res) {
 	res.render("index");
 });
+
+app.get("/products", function (req, res) {
+	res.render("products");
+})
+
+app.get("/services", function (req, res) {
+	res.render("services");
+})
 
 var port = process.env.PORT || 3000;
 app.listen(port, () => {
